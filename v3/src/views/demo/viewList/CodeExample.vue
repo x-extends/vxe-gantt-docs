ae1e1dfb4c4903b0ec4b33b7b1a273d33cd03ab3
xuliangzhan <xu_liangzhan@163.com>
2025-12-08 14:27:11 +0800

    update docs

--- v3/src/views/demo/viewList/CodeExample.vue
+++ v3/src/views/demo/viewList/CodeExample.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CodeLight path="demo/viewList/Demo1">
+    <CodeLight path="demo/viewList/Demo1" previewPath="/resource/pluginImg/demo_view_list.gif">
       <template #tip>
         <vxe-tip status="primary" title="多维视图切换">
         </vxe-tip>
