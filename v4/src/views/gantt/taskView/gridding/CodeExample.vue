97627069501656853f941a0bfeedf5f277b85abe
xuliangzhan <xu_liangzhan@163.com>
2025-12-18 20:54:56 +0800

    update docs

---
+++ v4/src/views/gantt/taskView/gridding/CodeExample.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <CodeLight path="gantt/taskView/gridding/Demo1">
+      <template #tip>
+        <vxe-tip status="primary" title="网格样式">
+          通过 <ApiLink name="gantt" prop="task-view-config"/>.<ApiLink name="gantt" prop="gridding"/>.<ApiLink name="gantt" prop="leftSpacing"/>、<ApiLink name="gantt" prop="task-view-config"/>.<ApiLink name="gantt" prop="gridding"/>.<ApiLink name="gantt" prop="rightSpacing"/> 设置网格的左右间隔列
+        </vxe-tip>
+      </template>
+    </CodeLight>
+  </div>
+</template>
